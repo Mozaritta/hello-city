1746502459bcaee4c6fd4bf0639f448b1cf70ded
Mozaritta <mozaritta@gmail.com>
2021-08-09 11:00:02 +0100

    Add PHP config, Mke Pin entity timestampable, Listing on index by createdAt desc

---
+++ migrations/Version20210809090710.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210809090710 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'ADD created_at AND updated_at FIELDS TO pins TABLE';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE answers (id INT AUTO_INCREMENT NOT NULL, description VARCHAR(255) NOT NULL, type VARCHAR(40) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE questions (id INT AUTO_INCREMENT NOT NULL, description VARCHAR(500) NOT NULL, client_id INT NOT NULL, type VARCHAR(255) NOT NULL, answer_id INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE type (id INT AUTO_INCREMENT NOT NULL, radio TINYINT(1) NOT NULL, checkbox TINYINT(1) NOT NULL, text TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE pins ADD created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, ADD updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE answers');
+        $this->addSql('DROP TABLE questions');
+        $this->addSql('DROP TABLE type');
+        $this->addSql('ALTER TABLE pins DROP created_at, DROP updated_at');
+    }
+}
