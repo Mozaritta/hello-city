e07ed8a415f01d9971b01b9e5b2dc2ad0954ffcb
Mozaritta <mozaritta@gmail.com>
2021-08-13 18:19:15 +0100

    Creer la table users dans la base de donnees en utilisant la commandes sc docrine:migrations:migrate

--- migrations/Version20210813171006.php
+++ migrations/Version20210813171006.php
@@ -14,7 +14,7 @@ final class Version20210813171006 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Create users table and make it timestampable';
     }
 
     public function up(Schema $schema): void
