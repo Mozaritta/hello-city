baa1eaf77442df7051d3eddb2071ba3e99c64243
Mozaritta <mozaritta@gmail.com>
2021-08-16 13:55:29 +0100

    Some old js code

--- assets/app.js
+++ assets/app.js
@@ -13,20 +13,11 @@ import './bootstrap';
 import $ from 'jquery';
 import 'bootstrap';
 
-$(document).ready(function() {
-    $("#success-alert").hide();
-    $("#myWish").click(function showAlert() {
-      $("#success-alert").fadeTo(2000, 500).slideUp(500, function() {
-        $("#success-alert").slideUp(500);
-      });
-    });
-  });
-
-  $(document).ready(function() {
-    $("#danger-alert").hide();
-    $("#myWish").click(function showAlert() {
-      $("#danger-alert").fadeTo(2000, 500).slideUp(500, function() {
-        $("#danger-alert").slideUp(500);
-      });
-    });
-  });
+// $(document).ready(function() {
+//     $("#success-alert").hide();
+//     $("#myWish").click(function showAlert() {
+//       $("#success-alert").fadeTo(2000, 500).slideUp(500, function() {
+//         $("#success-alert").slideUp(500);
+//       });
+//     });
+//   });
--- public/js/alert.js
+++ public/js/alert.js
@@ -1,17 +1,17 @@
-$(document).ready(function() {
-    $("#success-alert").hide();
-    $("#myWish").click(function showAlert() {
-      $("#success-alert").fadeTo(2000, 500).slideUp(500, function() {
-        $("#success-alert").slideUp(500);
-      });
-    });
-  });
+// $(document).ready(function() {
+//     $("#success-alert").hide();
+//     $("#myWish").click(function showAlert() {
+//       $("#success-alert").fadeTo(2000, 500).slideUp(500, function() {
+//         $("#success-alert").slideUp(500);
+//       });
+//     });
+//   });
 
-  $(document).ready(function() {
-    $("#danger-alert").hide();
-    $("#myWish").click(function showAlert() {
-      $("#danger-alert").fadeTo(2000, 500).slideUp(500, function() {
-        $("#danger-alert").slideUp(500);
-      });
-    });
-  });
+  // $(document).ready(function() {
+  //   $("#danger-alert").hide();
+  //   $("#myWish").click(function showAlert() {
+  //     $("#danger-alert").fadeTo(2000, 500).slideUp(500, function() {
+  //       $("#danger-alert").slideUp(500);
+  //     });
+  //   });
+  // });
